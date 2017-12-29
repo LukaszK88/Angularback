b67259ac4c05119d00626b0cdd98896e370bbb04
LukaszK88 <lukaskowalpl@yahoo.co.uk>
2017-12-29 10:18:38 +0000

    email template changes

--- resources/views/emails/club-active.blade.php
+++ resources/views/emails/club-active.blade.php
@@ -10,6 +10,23 @@ Once logged in you can take control of your Club profile<br>
 -Add ranking records (Club page / Ranking category pages)<br>
 and more stuff...
 
+Ranking Record guidelines:<br>
+Make sure you only record proper tournaments, should be self explanatory not to record tournament in your back garden..
+Cheatsheet:<br>
+Bohurt(each round):<br>
+won and standing - Fighters team won and he is on his feet.<br>
+last man standing - Fighters team lost but he was the last man standing<br>
+suicide - we all know this guy...<br>
+
+Profight(only proper profights 3x3min on official events or first class (FC):<br>
+KO - victory by KO<br>
+win - 3x3min profight<br>
+FC I - 1t place at first class event<br>
+FC II - 2nd place at first class event<br>
+FC III - 3rd place at first class <br>
+
+Rest of the categories works on win/loss basis (matches/fights not rounds)<br>
+
 
 
 Thanks,<br>
